0c83e8472921a58b6cff82b47f26405902940921
merve <1merveakcakaya@gmail.com>
2021-12-09 22:31:20 +0300

    merve commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassStructer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
