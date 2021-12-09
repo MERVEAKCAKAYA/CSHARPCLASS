278569c7145308d4fd1578477a59c6d5bce3eab0
MERVEAKCAKAYA <72937913+MERVEAKCAKAYA@users.noreply.github.com>
2021-12-09 23:19:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+///SINIF KAVRAMI
+/*using System;
 
 namespace ClassStructer
 {
@@ -6,7 +7,42 @@ namespace ClassStructer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calisan calisan1 = new Calisan();
+            calisan1.ad="MERVE";
+            calisan1.soyad="AKÇAKAYA";
+            calisan1.numara=2549;
+            calisan1.departman="IT";
+            calisan1.CalisanBilgi();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.ad="BAMBAM";
+            calisan2.soyad="AKÇAKAYA";
+            calisan2.numara=2547;
+            calisan2.departman="MAMA YEME";
+            calisan2.CalisanBilgi();
+
+
+
+        }
+    }
+
+
+    class Calisan{
+        public string ad;
+        public string soyad;
+        public int numara;
+        public string departman;
+
+        public void CalisanBilgi(){
+            Console.WriteLine("Calisan adi: {0}",ad);
+            Console.WriteLine("Calisan soyadi: {0}",soyad);
+            Console.WriteLine("Calisan numarasi: {0}",numara);
+            Console.WriteLine("Calisan departmani: {0}",departman);
+
         }
     }
 }
+*/
+
+
+
